c894908a3dd53b829c48fac09087c8f09a092af3
mysticslippers <eternal.martyr13579@gmail.com>
2024-04-19 11:31:32 +0300

    done RequestHandler

--- server/src/main/java/me/ifmo/server/utils/RequestHandler.java
+++ server/src/main/java/me/ifmo/server/utils/RequestHandler.java
@@ -3,8 +3,10 @@ package me.ifmo.server.utils;
 import me.ifmo.common.interaction.Request;
 import me.ifmo.common.interaction.Response;
 import me.ifmo.common.interaction.ResponseCode;
+import me.ifmo.server.utils.commands.Command;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public class RequestHandler {
     private final CommandManager commandManager;
@@ -13,4 +15,29 @@ public class RequestHandler {
         this.commandManager = commandManager;
     }
 
+    public Response handleRequest(Request request){
+        String nameOfCommand = request.getCommandName();
+        String argumentOfCommand = request.getArgumentOfCommand();
+        Serializable transmittedObject = request.getTransmittedObject();
+        ResponseCode responseCode = getResponseCode(nameOfCommand, argumentOfCommand, transmittedObject);
+        return new Response(ResponseBodyFormatter.getResponse(), responseCode);
+    }
+
+    public ResponseCode getResponseCode(String nameOfCommand, String argumentOfCommand, Serializable transmittedObject){
+        Map<String, Command> commands = this.commandManager.getCommands();
+        if(commands.containsKey(nameOfCommand)){
+            Command inputCommand = commands.get(nameOfCommand);
+            if(inputCommand.hasValidArgument(argumentOfCommand, transmittedObject)){
+                inputCommand.execute();
+                if(nameOfCommand.equals("exit")) return ResponseCode.DISCONNECTED;
+                return ResponseCode.EXECUTED;
+            } else {
+                return ResponseCode.ERROR;
+            }
+        } else {
+            ResponseBodyFormatter.addResponseText("----------------------");
+            ResponseBodyFormatter.addResponseText("Wrong command input!");
+            return ResponseCode.ERROR;
+        }
+    }
 }
