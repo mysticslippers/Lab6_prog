649bf4b9fbccf4e54ebbefe6a4d3a82354861ba0
misticslippers <eternal.martyr13579@gmail.com>
2024-07-31 16:40:28 +0300

    add ServerApp.java

--- server/src/main/java/me/ifmo/server/ServerApp.java
+++ server/src/main/java/me/ifmo/server/ServerApp.java
@@ -1,7 +1,16 @@
 package me.ifmo.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Objects;
+
 public class ServerApp {
+    public static Logger logger = LogManager.getLogger("Server");
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if(Objects.equals(args[0], "CollectionCSV.csv") || Objects.equals(args[0], "CollectionJSON.json") || Objects.equals(args[0], "CollectionXML.xml")){
+            ServerLaunchManager serverLaunchManager = new ServerLaunchManager(args[0]);
+            serverLaunchManager.launch();
+        }
     }
 }
